784a54a2817903367231be8a4743d727c73f19fb
Santiagitk10 <93751091+Santiagitk10@users.noreply.github.com>
2022-11-03 11:40:39 -0500

    notes

--- spring-webflux/src/main/java/com/stackdev/springwebflux/SpringWebfluxApplication.java
+++ spring-webflux/src/main/java/com/stackdev/springwebflux/SpringWebfluxApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
+//Se necesita habilitar el repositorio R2dbc
 @SpringBootApplication
 @EnableR2dbcRepositories
 public class SpringWebfluxApplication {
